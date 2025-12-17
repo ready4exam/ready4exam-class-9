0b42f15a74b658bedf281741bf15f58eb89c6428
ready4exam <ready4urexam@gmail.com>
2025-12-17 12:47:03 +0530

    chore: update table_id for Lines and Angles → mathematics_lines_angles_9_quiz

--- js/curriculum.js
+++ js/curriculum.js
@@ -223,7 +223,7 @@ export const curriculum = {
       },
       {
         "chapter_title": "Lines and Angles",
-        "table_id": "",
+        "table_id": "mathematics_lines_angles_9_quiz",
         "section": "Mathematics"
       },
       {
